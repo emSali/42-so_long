380ff3e32b2bc64e9621f365bbec1ccb40b6386b
Emily <you@example.com>
2023-03-04 09:28:52 +0000

    show window

--- srcs/solong.c
+++ srcs/solong.c
@@ -12,17 +12,46 @@
 
 #include "solong.h"
 
-int	main(void)
+/*
+t_img	new_img(int w, int h, t_win window)
+{
+	t_img	image;
+
+	image.win = window;
+	image.img_ptr = mlx_new_image(window.mlx_ptr, w, h);
+	image.addr = mlx_get_data_addr(image.img_ptr, &(image.bpp),
+			&(image.line_len), &(image.endian));
+	image.w = w;
+	image.h = h;
+	return (image);
+}
+*/
+
+t_win new_window(int w, int h, char *str)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
 
-	mlx_ptr = mlx_init();
-	if (!mlx_ptr)
+	mlx_ptr = mlx_init(); //create program instance
+	win_ptr = mlx_new_window(mlx_ptr, w, h, str); //window pointer -> instance of window
+	return ((t_win) {mlx_ptr, win_ptr, w, h});
+}
+
+
+int	main(void)
+{
+	t_win	win;
+
+	win = new_window(300, 300, "Game");
+	if (!win.mlx_ptr || !win.win_ptr)
 		return (1);
-	win_ptr = mlx_new_window (mlx_ptr, 300, 300, "Hello World");
-	if (!win_ptr)
-		return (2);
-	mlx_loop(mlx_ptr);
+
+	//t_img	image;
+
+	//image.win = win;
+	//image.
+
+	mlx_loop(win.mlx_ptr); //infinite loop to keep program running and window open
 	return (0);
+
 }
--- srcs/solong.h
+++ srcs/solong.h
@@ -16,5 +16,28 @@
 #include "../mlx_linux/mlx.h"
 #include "../libft/libft.h"
 
+typedef struct s_win // struct representing window
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		height;
+	int		width;
+}		t_win;
+
+typedef struct s_img // struct representing image
+{
+	t_win	win; //window
+	void	*img_ptr; //address of mlx image instance
+	char	*addr; //adress, where chain of bytes starts
+	int		h; //image-height
+	int		w; //image-width
+	int		bpp; //bits per pixel
+	int		endian; // 
+	int		line_len;
+}		t_img;
+
+
+t_win new_window(int w, int h, char *str);
+
 #endif
                                
